159a3cf1f3e782c368c8ef5461705323d345ef03
Edw10 <edward-1094@hotmail.com>
2021-04-22 03:22:21 +0000

    add

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -13,7 +13,7 @@ def canUnlockAll(boxes):
     if 0 in o_b:
         o_b.remove(0)
     if any(x not in o_b for x in range(1, len(boxes))):
-        if any(x not in range(1, len(boxes)) for x in o_b):
-               return False
+        return False
+    if any(x not in range(1, len(boxes)) for x in o_b):
         return False
     return True
