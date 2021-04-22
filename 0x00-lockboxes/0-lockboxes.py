01381b10581242b5a2cd047727fe8b75302c4a9e
Edw10 <edward-1094@hotmail.com>
2021-04-22 02:11:50 +0000

    add

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,9 +6,10 @@ def canUnlockAll(boxes):
     """ function to determine if the boxes can be open  """
     o_b = [0]
     for y in o_b:
-        for i in boxes[y]:
-            if i not in o_b:
-                o_b.append(i)
+        if y <= len(boxes):
+            for i in boxes[y]:
+                if i not in o_b:
+                    o_b.append(i)
     if all(x in o_b for x in range(0, len(boxes))):
         return True
     return False
