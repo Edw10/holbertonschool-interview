ada5f52d2d75d666418f94390eff6a9a31c8adfd
Edw10 <edward-1094@hotmail.com>
2021-05-21 04:02:08 +0000

    add

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -9,19 +9,19 @@
  */
 static void print_grid(int grid[3][3])
 {
-  int i, j;
+	int i, j;
 
-  printf("=\n");
-  for (i = 0; i < 3; i++)
-    {
-      for (j = 0; j < 3; j++)
+	printf("=\n");
+	for (i = 0; i < 3; i++)
 	{
-	  if (j)
-	    printf(" ");
-	  printf("%d", grid[i][j]);
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
 	}
-      printf("\n");
-    }
 }
 
 /**
@@ -32,39 +32,39 @@ static void print_grid(int grid[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-  int i, j, print, temp[3][3];
+	int i, j, print, temp[3][3];
 
-  if (!grid1 || !grid2)
-    return;
+	if (!grid1 || !grid2)
+		return;
 
-  for (i = 0; i < 3; i++)
-    for (j = 0; j < 3; j++)
-      grid1[i][j] += grid2[i][j];
-  do {
-    for (i = 0; i < 3; i++)
-      for (j = 0; j < 3; j++)
-	temp[i][j] = grid1[i][j];
-    print = 0;
-    for (i = 0; i < 3; i++)
-      {
-	for (j = 0; j < 3; j++)
-	  {
-	    if (temp[i][j] > 3)
-	      {
-		if (!print)
-		  print_grid(grid1);
-		print = 1;
-		grid1[i][j] -= 4;
-		if (i > 0)
-		  grid1[i - 1][j] += 1;
-		if (i < 2)
-		  grid1[i + 1][j] += 1;
-		if (j > 0)
-		  grid1[i][j - 1] += 1;
-		if (j < 2)
-		  grid1[i][j + 1] += 1;
-	      }
-	  }
-      }
-  } while (print);
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid1[i][j] += grid2[i][j];
+	do {
+		for (i = 0; i < 3; i++)
+			for (j = 0; j < 3; j++)
+				temp[i][j] = grid1[i][j];
+		print = 0;
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				if (temp[i][j] > 3)
+				{
+					if (!print)
+						print_grid(grid1);
+					print = 1;
+					grid1[i][j] -= 4;
+					if (i > 0)
+						grid1[i - 1][j] += 1;
+					if (i < 2)
+						grid1[i + 1][j] += 1;
+					if (j > 0)
+						grid1[i][j - 1] += 1;
+					if (j < 2)
+						grid1[i][j + 1] += 1;
+				}
+			}
+		}
+	} while (print);
 }
