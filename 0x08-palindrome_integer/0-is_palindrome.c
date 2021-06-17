8444acf7c8be188130f7d8bb434e558cd3d0a013
Edw10 <edward-1094@hotmail.com>
2021-06-17 20:49:43 +0000

    add

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,18 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - Checks if is palindrome
+ * @n: Number to be checked.
+ * Return: 1 if n is a palindrome, and 0 otherwise.
+ */
+int is_palindrome(unsigned long n)
+{
+	unsigned long copy = n, new = 0;
+
+	while (copy)
+	{
+		new = new * 10 + copy % 10;
+		copy /= 10;
+	}
+	return (n == new);
+}
