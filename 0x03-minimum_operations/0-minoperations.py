f5bf41fb1598db6717e953eeffaa919c61ea9e49
Edward Osorno Muñoz <55111692+Edw10@users.noreply.github.com>
2021-10-22 14:08:56 -0500

    add

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Minimum operations"""
+
+
+def minOperations(n):
+    if type(n) is not int or n <= 1:
+        return 0
+    nH = 1
+    copy = 1
+    operations = 1
+    while nH < n:
+        if n % nH == 0 and nH != copy:
+            copy = nH
+            operations += 1
+        nH = nH + copy
+        operations += 1
+    if nH == n:
+        return operations
+    return 0
